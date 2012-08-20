9d692065ca8fe5deff897a7f363f2a9e4631b923
uglymunky <gabriel@uglymunky.com>
2012-08-20 00:43:06 -0700

    initial commit - finally making some progress

---
+++ js/kv.form/js/kv.form.js
@@ -0,0 +1,5 @@
+$('.fSetMsg').css('display', 'inline-block');
+
+// @todo make wrap all elements except the first with .fSetInner
+
+$('input[type="checkbox"], input[type="radio"]').parent('.fSet').addClass('fCSet')
