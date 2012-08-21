ad731d16534a03be99497bdfe08ae33f68c7b1fe
uglymunky <gabriel@uglymunky.com>
2012-08-20 18:08:14 -0700

    Updates * add logic to wrap with .fSetInner * clean up demo page * demo .fSetGroup

--- js/kv.form/js/kv.form.js
+++ js/kv.form/js/kv.form.js
@@ -1,5 +1,42 @@
-$('.fSetMsg').css('display', 'inline-block');
+// use this to wrap the inner (not including the first element)
+var $fSetInner = $('<div class="fSetInner" />');
 
-// @todo make wrap all elements except the first with .fSetInner
+
+
+$('.kvForm').css('display', 'block');
 
 $('input[type="checkbox"], input[type="radio"]').parent('.fSet').addClass('fCSet')
+
+// Set required class
+$('input[required]').parent('.fSet').addClass('required');
+
+
+/**
+
+ get all non-first child elements of an fSet that are either:
+ label
+ input
+
+
+**/
+
+/**
+ * Wraps all non-first child elements of an fSet with an .fSetInner so long as they are an:
+ * select
+ * input
+ * textarea
+ * label
+ */
+$('.fSet').each(function () {
+	var $children = $(this).children('select, input, textarea, label')
+	, $firstEl = $children.first()
+	, $wrapMe = $children.not($firstEl)
+	, $wrapper;
+
+	if ($wrapMe.length > 0) {
+		$wrapMe.remove();
+		$wrapper = $wrapMe.wrapAll($fSetInner).parent();
+
+		$firstEl.after($wrapper);
+	}
+});
