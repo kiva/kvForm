55e1095e1beb72720eefd3529869c9103065e0d9
uglymunky <gabriel@uglymunky.com>
2012-08-21 23:32:35 -0700

    Major shifting around/renaming

--- js/kv.form/js/kv.form.js
+++ js/kv.form/js/kv.form.js
@@ -1,42 +1,354 @@
-// use this to wrap the inner (not including the first element)
-var $fSetInner = $('<div class="fSetInner" />');
 
 
 
-$('.kvForm').css('display', 'block');
-
-$('input[type="checkbox"], input[type="radio"]').parent('.fSet').addClass('fCSet')
-
-// Set required class
-$('input[required]').parent('.fSet').addClass('required');
 
 
-/**
 
- get all non-first child elements of an fSet that are either:
- label
- input
+(function () {
+	'use strict';
+
+	// use this to wrap the inner (not including the first element)
+	var $fSetInner = $('<div class="fSetInner" />')
+	, _ajaxForm = $.fn.ajaxForm;
+
+	delete $.fn.ajaxForm;
 
 
-**/
+	/**
+	 * Checks/unchecks all checkboxes that correspond to a given "massToggler"
+	 *
+	 * @param {jQuery} $massToggler
+	 * @param {jQuery} $checkboxes
+	 */
+	function checkMassToggler ($massToggler, $checkboxes) {
+		var checkedCounter = 0;
 
-/**
- * Wraps all non-first child elements of an fSet with an .fSetInner so long as they are an:
- * select
- * input
- * textarea
- * label
- */
-$('.fSet').each(function () {
-	var $children = $(this).children('select, input, textarea, label')
-	, $firstEl = $children.first()
-	, $wrapMe = $children.not($firstEl)
-	, $wrapper;
+		$checkboxes.each(function () {
+			if (this.checked) {
+				checkedCounter++;
+			}
+		});
 
-	if ($wrapMe.length > 0) {
-		$wrapMe.remove();
-		$wrapper = $wrapMe.wrapAll($fSetInner).parent();
-
-		$firstEl.after($wrapper);
+		if (checkedCounter === 0) {
+			$massToggler.prop('checked', false).change().removeClass('someChecked');
+		} else if ($checkboxes.length === checkedCounter) {
+			$massToggler.prop('checked', true).change().removeClass('someChecked');
+		} else {
+			$massToggler.prop('checked', true).change().addClass('someChecked');
+		}
 	}
-});
+
+
+	/**
+	 * @param {jQuery} $nestedSet
+	 * @param {jQuery} $nestedFields
+	 * @param {jQuery} $displayToggler
+	 */
+	function showNestedSet ($nestedSet, $nestedFields, $displayToggler) {
+		$nestedFields.stop().slideDown(function () {
+			$displayToggler.text($displayToggler.text().replace('advanced', 'close'));
+		});
+		$nestedSet.addClass(openClass);
+	}
+
+
+	/**
+	 * @param {jQuery} $nestedSet
+	 * @param {jQuery} $nestedFields
+	 * @param {jQuery} $displayToggler
+	 */
+	function hideNestedSet ($nestedSet, $nestedFields, $displayToggler) {
+		$nestedFields.stop().slideUp(function () {
+			$displayToggler.text($displayToggler.text().replace('close', 'advanced'));
+		});
+		$nestedSet.removeClass(openClass);
+	}
+
+
+
+	/**
+	 *
+	 * @param {Object} options - You MUST either pass the nestedSet or the targetSet property
+	 * nestedSet
+	 * targetSet
+	 * displayToggler [optional]
+	 */
+	function toggleExpandableSet(options) {
+		var $nestedSet
+		, $targetSet
+		, $nestedFields
+		, $displayToggler;
+
+		if (options.nestedSet) {
+			$nestedSet = $(options.nestedSet);
+			$nestedFields = $('> .nSetHead + *', $nestedSet);
+		} else if (options.targetSet) {
+			$targetSet = $(options.targetSet);
+			$nestedFields = $targetSet.parent();
+			$nestedSet = $nestedFields.closest('.nSet');
+		}
+
+		// Save a reference to the display toggler
+		$displayToggler = options.displayToggler
+			? $(options.displayToggler)
+			: $('.displayToggler', $nestedSet);
+
+		if ($nestedSet.hasClass(openClass)) {
+			hideNestedSet($nestedSet, $nestedFields, $displayToggler);
+		} else {
+			showNestedSet($nestedSet, $nestedFields, $displayToggler);
+		}
+	}
+
+	// Loop through and set up each "expandable set".
+	function setUpExpandableSet(index, el) {
+		var $displayToggler = $('<a href="#" class="displayToggler">advanced</a>')
+		, $massToggler = $('.massToggler', this).first()
+		, $checkboxes = $('> ul li input[type="checkbox"]', this);
+
+		// Set up the display toggler
+		$displayToggler.click(function (e) {
+			e.preventDefault();
+
+			toggleNestedSet({displayToggler: $displayToggler, nestedSet: $nestedSet});
+		});
+
+		// Set up the mass toggler, check or uncheck all related checkboxes
+		if ($checkboxes.length) {
+			$massToggler.click(function () {
+				$checkboxes.prop('checked', this.checked).change();
+			});
+		}
+
+		// Add the "displayToggler" to the DOM
+		$('> .nSetHead label', this).after($displayToggler);
+
+		$checkboxes.change(function () {
+			checkMassToggler($massToggler, $checkboxes);
+		});
+
+		checkMassToggler($massToggler, $checkboxes);
+	}
+
+
+	// Wraps all non-first select, input, textarea, and label child elements of an fSet with an .fSetInner
+	function innerWrap(index, el) {
+		var $children = $(el).children('select, input, textarea, label')
+		, $firstEl = $children.first()
+		, $wrapMe = $children.not($firstEl)
+		, $wrapper;
+
+		// Bundle all $wrapMe elements into one and add them back into the dom
+		if ($wrapMe.length > 0) {
+			$wrapMe.remove();
+			$wrapper = $wrapMe.wrapAll($fSetInner).parent();
+
+			$firstEl.after($wrapper);
+		}
+	}
+
+
+	function KvForm($targetForm, options) {
+		$targetForm = $($targetForm).css('display', 'block');
+
+		var $elements = $('input, select, textarea', $targetForm)
+		, $clickableElements = $('input[type="checkbox"], input[type="radio"]', $targetForm)
+		, $requiredElements = $('[required]', $targetForm)
+		, $sets = $('.fSet', $targetForm)
+		, $expandableSets = $('.expandableSet', $targetForm)
+		, formValidationObj;
+
+		// Wrap inner elements
+		$sets.each(innerWrap);
+
+		// Set required fSet
+		$requiredElements.closest('.fSet').addClass('required');
+
+		// Set clickable fSet
+		$clickableElements.closest('.fSet').addClass('clickableSet');
+
+		// Set expandable fSets
+		$expandableSets.each(setUpExpandableSet);
+
+		if (typeof $.fn.ajaxForm == 'function') {
+			_ajaxForm.call($targetForm, options);
+		}
+
+		if (typeof kv.formValidation  == 'function' && options.validate) {
+			formValidationObj = kv.formValidation(options.validation);
+
+			if (formValidationObj) {
+				// merge the object into the kvFormObject
+			}
+		}
+
+		return {
+			settings: options
+		}
+	}
+
+
+	function kvForm($targetForm, options) {
+		var kvForm = $.data($targetForm[0], 'kv-form');
+
+		if (kvForm) {
+			return kvForm;
+		}
+
+		return new KvForm($targetForm, options)
+	}
+
+	kv.extend({form: kvForm});
+	$.fn.kvForm = kv.jqueryExtend('form');
+}());
+
+
+		// kv.form.settings() returns an object of settings
+		// kv.form.settings('expandable', false);
+		// kv.form.settings('validate', false);
+		// kv.form.settings('h5Shim', false);
+
+		// options.validate = {}
+
+		// options.expandable = {}
+/*
+
+		$myForm.kvForm({
+			dataType: ''
+			, data: {}
+			, beforeSerialize: {}
+			, beforeSubmit: {}
+			, clearForm: ''
+			, error: ''
+			, forceSync: ''
+			, iframe: ''
+			, iframeSrc: ''
+			, iframeTarget: ''
+			, replaceTarget: ''
+			, resetForm: ''
+			, semantic: ''
+			, success: ''
+			, target: ''
+			, type: ''
+			, uploadProgress: ''
+			, url: ''
+			, validation: {
+
+			}
+			, expandableSets: true
+		});
+
+		if ($myForm.data('kv-form').settings('validation') && kv.formValidation) {
+			$myForm.kvValidate($myForm.data('kv-form').settings('validation'));
+		}
+
+		if ($myForm.data('kv-form').settings('expandable') && kv.formExpanableSets) {
+			$myForm.kvExpandableSets();
+		}
+
+		// These should all be readonly properties, except maybe settings
+		return {
+			id: 82892
+			, settings: options
+			, elements: {}
+			, groups: {}
+		};
+
+		// The validation and form objects get merged into one kvForm object when returned
+		// Otherwise, it is possible to call kv.formValidation() directly and get a kv.formValidation object
+
+
+		kv.form.enabled.expandable
+		kv.form.enabled.validate
+
+		kv.formExpandable
+		kv.formValidate
+	}
+
+
+	// listen for validation request event
+	kv.subscribe('form-ready', function () {
+		if ($myForm.data('kv-form').settings('validation')) {
+
+		}
+
+	});
+*/
+
+
+/*
+
+Use:
+
+$(myForm).kvForm(settings);
+
+OR:
+
+kv.form('.myForm', settings);
+
+settings: {
+	enableExpand: boolean [true by default]
+	,
+
+}
+
+
+You can pass kv.form a kv.validate instance or a plain js object
+
+passed into kv.form:
+
+{
+	name:
+}
+
+Instance of kv.form:
+{
+	id: 829
+	, validation: kvValidationObject
+	, formEl: FORMElement <- I say no need for the regular html element?
+	, $formEl: $FORMElement
+	, elements: [
+		{el: HTMLElement
+			, $el: $HTMLElement
+			, state: 'undefined', 'hasError', 'valid'
+			, type: corresponds to an HTML5 type, can be manually set via js, or gets it from the html element
+			, isRequired: true or false
+			, value: ''
+			, onErrorMsg: ''
+			, onSuccessMsg: ''
+			, helpMsg: ''
+		}
+	]
+	, groups: {
+		groupName: {
+			groupEl: HTMLElement
+			, $groupEl: $HTMLElement
+		}
+	}
+
+}
+
+Instance of kv.validate
+{
+	id: 3738
+	, state: 'undefined', 'hasError', 'valid' , or 'submitFailed'
+	, elements: [
+		{
+			state: 'undefined', 'hasError', 'valid'
+			, onErrorMsg: ''
+			, onSuccessMsg: ''
+		}
+	]
+	, groups: [
+		{
+
+		}
+	]
+}
+*/
+
+
+
+
+
+
---
+++ js/kv.form/test/spec/kv.form.js
@@ -0,0 +1,42 @@
+describe('kv.form', function() {
+	'use strict';
+
+	var $form;
+
+
+	beforeEach(function () {
+		$form = $(' <form class="kvForm fStack"><div class="fSet"><label>I1</label><input type="text" required/></div><div class="fSet valid"><label>I2</label><input type="text" /></div><div class="fSet"><label>I3</label><input type="text" /></div><div class="fSet"><div class="fSet"><label>Input 1</label><input type="text" /></div><div class="fSet"><label>Input 1</label><input type="text" /></div><div class="fSet"><label>Input 2</label><input type="text" /><div class="fSetMsg"><div>This message is for testing</div></div></div><div class="fSet"><label>Input 2</label><input type="text" /><div class="fSetMsg"><div>This message only shows when either js fails/is disabled or wrapped via js with a .fControl element</div></div></div></div><div class="fSet"><div class="fSetHead">Select one of the following checkboxes</div><div class="fSet"><input type="checkbox" id="checkbox1" /><label for="checkbox1">Label for checkbox 1</label></div><div class="fSet"><input type="checkbox" id="checkbox2" /><label for="checkbox2">Label for checkbox 2</label></div><div class="fSet"><input type="checkbox" id="checkbox3" /><label for="checkbox3">Label for checkbox 3</label></div><div class="fSet"><input type="checkbox" id="checkbox4" /><label for="checkbox4">Label for checkbox 4</label></div></div><div class="fSet"><div class="fSet fSetHead"><input type="checkbox" class="massToggler" /><label>Select one of the following checkboxes</label></div><div class="fSet"><input type="checkbox"/><label>LLLLabel for checkbox 1</label></div><div class="fSet"><input type="checkbox"/><label>Label for checkbox 2</label></div><div class="fSet"><input type="checkbox"/><label>Label for checkbox 3</label></div><div class="fSet"><input type="checkbox"/><label>Label for checkbox 4</label></div></div><div class="fSet fNSet"><div class="fSetHead">Header for an fSetHead</div><div class="fSet"><label>Some label</label><input type="text" /><input type="text" /></div></div><div class="fSet fNSet"><div class="fSet fSetHead"><input type="checkbox" class="massToggler" /><label>Header for an fSetHead + fSet</label><div class="fSetMsg"><div>Testing message in the header</div></div></div><div class="fSet fSliced"><div class="fSet"><input type="checkbox" id="checkbox6" /><label for="checkbox6">Label for checkbox 6</label><select><option>Select box</option></select><input type="text" /><input type="text" /><div class="fSetMsg"><div>Message for a checkbox item 1</div><div>Message for a checkbox item 2</div><div>Message for a checkbox item 3</div></div></div><div class="fSet"><input type="checkbox" id="checkbox7" /><label for="checkbox7">Label for checkbox 7</label></div><div class="fSet"><input type="checkbox" id="checkbox8" /><label for="checkbox8">Label for checkbox 8</label></div><div class="fSet"><div class="fSet"><input type="checkbox" id="checkbox9" /><label for="checkbox9">Label for checkbox 9</label></div><div class="fSet"><div class="fSet"><input type="checkbox" id="nestedCheckbox1" /><label for="checkbox1">Label for nestedCheckbox 1</label></div><div class="fSet"><input type="checkbox" id="nestedCheckbox2" /><label for="checkbox2">Label for nestedCheckbox 2</label></div><div class="fSet"><input type="checkbox" id="nestedCheckbox3" /><label for="checkbox3">Label for nestedCheckbox 3</label></div><div class="fSet"><input type="checkbox" id="nestedCheckbox4" /><label for="checkbox4">Label for nestedCheckbox 4</label><div class="fSetMsg"><div>Message for a checkbox item 1</div><div>Message for a checkbox item 2</div></div></div></div></div></div></div><div class="fSet fSliced"><div class="fSet"><input type="radio" name="a" /><label>Label for radio 1</label><div class="fSetMsg"><div>testing testing</div></div></div><div class="fSet"><input type="checkbox" /><label>Label for nestedCheckbox 4</label><div class="fSetMsg"><div>Message for a checkbox item 1</div><div>Message for a checkbox item 2</div></div></div><div class="fSet"><input type="radio" name="a" /><label>Label for radio 2</label></div><div class="fSet"><input type="radio" name="a" /><label>Label for radio 3</label></div></div><div class="fSet fSetGroup"><div class="fSetHead">Grouped inputs</div><div class="fSet"><label>first name</label><input type="text"></div><div class="fSet"><label>last name</label><input type="text"></div></div><div class="fSet fSetGroup"><div class="fSetHead">Grouped inputs</div><div class="fSet"><label>first name</label><input type="text"></div><div class="fSet"><label>last name</label><input type="text"></div><div class="fSetMsg"><div>This is my group message</div><div>This is my group message</div><div>This is my group message</div></div></div></form>').appendTo('body');
+		$form.kvForm();
+	});
+
+
+	afterEach(function () {
+		$form.remove();
+	});
+
+
+	it('extends the kv object with the "form" method', function () {
+		expect(typeof kv.form).toBe('function');
+	});
+
+
+	it('extends the kv prototype with the "kvForm" method', function () {
+		expect(typeof $.fn.kvForm).toBe('function');
+	});
+
+
+	it('sets the class, "required", on all .fSet parents of a required form element', function () {
+		var $parentFSet = $('[required]', $form).closest('.fSet');
+
+		$parentFSet.each(function () {
+			expect($(this).hasClass('required')).toBe(true);
+		});
+	});
+
+
+	it('sets the class, "fCSet" on all .fSet parents of a checkbox or radio form element', function () {
+		var $parentFSet = $('[type="radio"], [type="checkbox"]', $form).closest('.fSet');
+
+		expect($parentFSet.hasClass('clickableSet')).toBe(true);
+	});
+});
