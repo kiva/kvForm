a4b3bfbf303c2f0d5d0f7b89057c0f9d638fa24b
uglymunky <gabriel@uglymunky.com>
2012-09-05 14:03:16 -0700

    add stuff

--- js/kv.form-FormMsg/js/kv.formMsg.js
+++ js/kv.form-FormMsg/js/kv.formMsg.js
@@ -1,27 +1,27 @@
 (function ($, kv, global, undefined) {
 
-	kv.FormMsg = function (kvFormObj) {
+	kv.formMsg = function (kvFormObj) {
 		//** Set up the html + css **//
 
 	};
 
 
-	kv.Form.prototype.showMsgs = function () {
+	kv.form.prototype.showMsgs = function () {
 
 	}
 
 
-	kv.Form.prototype.hideMsgs = function () {
+	kv.form.prototype.hideMsgs = function () {
 
 	}
 
 
-	kv.Form.prototype.showMsg = function () {
+	kv.form.prototype.showMsg = function () {
 
 	};
 
 
-	kv.Form.prototype.hideMsg = function () {
+	kv.form.prototype.hideMsg = function () {
 
 	};
 
